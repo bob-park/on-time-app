851ca4bbc9bc195c6bb4fc2d5f3a4bf78741ca1e
Bob Park <hwpark@malgn.com>
2025-11-11 13:21:11 +0900

    build: eslint v9 적용

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+// eslint.config.mjs
+import eslintConfig from '@bob-park/eslint-config-bobpark';
+
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    extends: [eslintConfig],
+  },
+]);
